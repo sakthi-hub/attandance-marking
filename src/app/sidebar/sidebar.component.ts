d0fd0e4d6fec6d72b07a8d37aae30c44e80be9cd
sakthi-hub <52353312+sakthi-hub@users.noreply.github.com>
2019-08-27 11:59:09 +0530

    Add files via upload

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
+    { path: '/user', title: 'User Profile',  icon:'pe-7s-user', class: '' },
+    { path: '/table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
+    { path: '/emp_details', title: 'Emp Details',  icon:'pe-7s-note2', class: '' },
+
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
